8cec0f912c79e09a898aa403a4a4b51514d6aa06
Lokeshsharmaa-01 <iamlokesh2024@gmail.com>
2024-11-20 09:20:19 +0530

    second

--- models/student.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const studentSchema = new mongoose.Schema({
-    name:{type: String, required: true}
-})
-
-export const student = mongoose.model("Student",studentSchema);
